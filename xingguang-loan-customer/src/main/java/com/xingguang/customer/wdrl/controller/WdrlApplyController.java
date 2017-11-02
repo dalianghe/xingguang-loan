b9e751d47048df4842df926298f9eb4397a72b77
zongxu <15116951695@163.com>
2017-11-02 09:56:05 +0800

    格式化

--- xingguang-loan-customer/src/main/java/com/xingguang/customer/wdrl/controller/WdrlApplyController.java
+++ xingguang-loan-customer/src/main/java/com/xingguang/customer/wdrl/controller/WdrlApplyController.java
@@ -10,7 +10,9 @@ import com.xingguang.customer.product.entity.ProductInfo;
 import com.xingguang.customer.product.service.IProductService;
 import com.xingguang.customer.product.service.IProductTermService;
 import com.xingguang.customer.wdrl.entity.WdrlApply;
+import com.xingguang.customer.wdrl.entity.WdrlApplyExample;
 import com.xingguang.customer.wdrl.service.IWdrlApplyService;
+import io.swagger.annotations.Example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by 宗旭 on 2017/10/01.
@@ -38,7 +41,7 @@ public class WdrlApplyController {
     private ICusUserInfoService userInfoService;
 
     @RequestMapping(value = "/wdrl/apply",method = RequestMethod.POST)
-    public ResultBean<?> createCreditApply(@RequestBody WdrlApply wdrlApply, @JWTParam(key = "userId", required = true) Long userId){
+    public ResultBean<?> apply(@RequestBody WdrlApply wdrlApply, @JWTParam(key = "userId", required = true) Long userId){
         ResultBean resultBean =  new ResultBean();
         CreditInfo creditInfo = this.creditInfoService.findByCusUserId(userId);
         if(ObjectUtils.isEmpty(creditInfo)){
@@ -53,13 +56,24 @@ public class WdrlApplyController {
         CusUserInfo cusUserInfo = this.userInfoService.findById(userId);
         wdrlApply.setCusUserId(userId);
         wdrlApply.setCusUserName(cusUserInfo.getName());
-        ProductInfo productInfo = this.productService.getProductById(wdrlApply.getProductId());
+        ProductInfo productInfo = this.productService.getProductById(creditInfo.getProductId());
         wdrlApply.setServiceCharge(wdrlApply.getAmount().multiply(productInfo.getServiceRate()));
         wdrlApply.setAccMgmtCharge(wdrlApply.getAmount().multiply(productInfo.getAccMgmtRate()));
+        wdrlApply.setProductId(creditInfo.getProductId());
+        wdrlApply.setTermId(creditInfo.getTermId());
         wdrlApply.setStatus(1);
         wdrlApply.setCreateTime(new Date());
         this.wdrlApplyService.create(wdrlApply);
         return resultBean;
+    }
+
+    @RequestMapping(value = "/wdrl/apply/list",method = RequestMethod.GET)
+    public ResultBean<?> getList(@JWTParam(key = "userId", required = true) Long userId){
+        WdrlApplyExample example = new WdrlApplyExample();
+        example.createCriteria().andCusUserIdEqualTo(userId);
+        example.setOrderByClause("create_time desc");
+        List<WdrlApply> list = this.wdrlApplyService.getWdrlApplyList(example);
+        return new ResultBean(list);
     }
 
 }
