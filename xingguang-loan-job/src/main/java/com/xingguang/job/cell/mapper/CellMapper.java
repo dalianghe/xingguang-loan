ea732ca6a4ecad7043e101050cd2ad4b5e693312
dalianghe <276420284@qq.com>
2017-11-09 10:26:27 +0800

    删除job工程

--- xingguang-loan-job/src/main/java/com/xingguang/job/cell/mapper/CellMapper.java
+++
@@ -1,7 +0,0 @@
-package com.xingguang.job.cell.mapper;
-
-/**
- * Created by admin on 2017/11/8.
- */
-public interface CellMapper {
-}
--- xingguang-loan-job/src/main/java/com/xingguang/job/cell/service/ICellService.java
+++
@@ -1,7 +0,0 @@
-package com.xingguang.job.cell.service;
-
-/**
- * Created by admin on 2017/11/8.
- */
-public interface ICellService {
-}
