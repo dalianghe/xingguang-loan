4fb2ce66e4992e3206e21491a4354e12876ccb6e
dalianghe <276420284@qq.com>
2017-11-11 17:39:50 +0800

    添加拉取报告任务

---
+++ xingguang-loan-task/src/main/java/com/xingguang/quartz/common/job/PullJxlReportJob.java
@@ -0,0 +1,26 @@
+package com.xingguang.quartz.common.job;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Description 用一句话描述该文件做什么
+ * @Author hedaliang
+ * @Date 2017/11/11  16:47
+ * @Version v1.0.0
+ */
+public class PullJxlReportJob implements Job {
+
+    Logger logger = LogManager.getLogger(getClass());
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        logger.info("拉取聚信立报告===>JobName: {}", context.getJobDetail().getKey().getName());
+        String url = "http://localhost:8081/cell/job";
+    }
+}
