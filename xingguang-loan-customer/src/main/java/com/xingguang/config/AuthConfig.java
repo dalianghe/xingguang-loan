2bdac1a0ef2cc05167f7114b7e5e8ed01386762d
zongxu <15116951695@163.com>
2017-11-13 19:28:23 +0800

    还款计划部分

--- xingguang-loan-customer/src/main/java/com/xingguang/config/AuthConfig.java
+++ xingguang-loan-customer/src/main/java/com/xingguang/config/AuthConfig.java
@@ -29,6 +29,7 @@ public class AuthConfig extends WebMvcConfigurerAdapter {
                 .excludePathPatterns("/verify/code")
                 .excludePathPatterns("/verify/code/*")
                 .excludePathPatterns("/sms/send/*")
+                .excludePathPatterns("/wx")
                 .excludePathPatterns("/wx/**");
         super.addInterceptors(registry);
     }
